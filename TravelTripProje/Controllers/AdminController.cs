ffe1e35e236489294663152c95c59e919d8a1bbd
duygu <duygukalayci80@gmail.com>
2025-04-14 17:51:24 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using TravelTripProje.Models.Sınıflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context context = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = context.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)    //Bu metod, kullanıcı formu doldurup gönderdiğinde (yani POST isteği yaptığında) çalışır.
+
+        {
+            context.Blogs.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //Kullanıcı /YeniBlog adresine gider(GET isteği), formu görür.
+        //Formu doldurur ve gönderir (POST isteği).
+        //Sunucu gelen verileri alır, veritabanına kaydeder ve kullanıcıyı Index sayfasına yönlendirir.
+
+        public ActionResult BlogSil(int id)//silme işlemini idye göre yapacağız.
+        {
+            var b = context.Blogs.Find(id);//idden gelen degeri bul.
+            context.Blogs.Remove(b);//bden gelen degeri sil.
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogGetir(int id)
+        {
+            var b1 = context.Blogs.Find(id);
+            return View("BlogGetir", b1);
+        }
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var blg=context.Blogs.Find(b.Id);
+            blg.Baslik = b.Baslik;
+            blg.Aciklama = b.Aciklama;
+            blg.BlogImage = b.BlogImage;
+            blg.Tarih = b.Tarih;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = context.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+
+        public ActionResult YorumSil(int id)
+        {
+            var b=context.Yorumlars.Find(id);
+            context.Yorumlars.Remove(b);
+            context.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+
+        public ActionResult YorumGetir(int id)
+        {
+            var y = context.Yorumlars.Find(id);
+            return View("YorumGetir", y);
+        }
+
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yrm = context.Yorumlars.Find(y.Id);
+            yrm.KullaniciAdi = y.KullaniciAdi;
+            yrm.Mail = y.Mail;
+            yrm.Yorum = y.Yorum;
+            context.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+
+       
+
+
+    }
+}
---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Sınıflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c=new Context();
+        BlogYorum by = new BlogYorum();
+        public ActionResult Index()
+        {
+           // var bloglar=c.Blogs.ToList();
+           by.Deger1 = c.Blogs.ToList();
+           by.Deger3 = c.Blogs.OrderByDescending(x => x.Id).Take(3).ToList();
+
+            return View(by);
+        }
+        /*
+         * Controller, gelen HTTP isteklerini alır, işleme yapar ve uygun sonuçları (view'lar veya başka veriler) geri döner.
+         * Context, muhtemelen bir veritabanı bağlamı (context) nesnesidir. Bu genellikle Entity Framework kullanarak veritabanı işlemleri yapmayı sağlar.
+         * Context sınıfı, veritabanı ile iletişim kurmak için kullanılır ve burada c adında bir örneği oluşturulmuştur.
+         * Bu nesne, veritabanındaki Blogs tablosuna erişimi sağlar.
+         * 
+         * c.Blogs ifadesi, veritabanındaki Blogs tablosunu temsil eder. Bu tablodaki tüm blogları almak için kullanılır.
+         * ToList() metodu, veritabanındaki Blogs tablosundaki tüm kayıtları liste haline getirir.
+         * Sonuçta, bloglar adında bir liste elde edilir ve bu listeyi View'a (görünüme) ileteceğiz.
+         * özetle;
+         * Bu controller, bir HTTP GET isteği aldığında veritabanından blogların listesini alır, bu listeyi Index görünümüne ileterek kullanıcıya blogları gösterir.
+         */
+
+        public ActionResult BlogDetay(int? id)
+        {
+            by.Deger1= c.Blogs.Where(x => x.Id == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.BlogId == id).ToList();
+            //var blogbul=c.Blogs.Where(x =>x.Id==id).ToList();
+            return View(by);
+        }
+        [HttpGet]
+        public PartialViewResult YorumYap(int id)
+        {
+            ViewBag.deger = id;
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult YorumYap(Yorumlar b)
+        {
+            c.Yorumlars.Add(b);
+            c.SaveChanges();
+            return PartialView();
+        }
+    }
+}
